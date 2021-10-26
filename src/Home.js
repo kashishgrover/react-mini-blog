52991f5759b3b4e141adfc9737cae80b5b6c7bf7
Varsha Singh <varsha.singh0497@gmail.com>
2021-10-26 17:19:16 +0530

    refactor: extract error handling into custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,9 @@
 import {useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then( res => {
-                if(!res.ok) {
-                    throw Error('could notfetch data for that resource');
-                }
-                return res.json();
-            })
-            .then((data) => { 
-                setBlogs(data);
-                setIsLoading(false);
-                setError(null);
-            })
-            .catch(error => {
-                setIsLoading(false);
-                console.log(error.message);
-            })
-    }, [])
+    const { data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import {useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch(url)
+            .then( res => {
+                if(!res.ok) {
+                    throw Error('could notfetch data for that resource');
+                }
+                return res.json();
+            })
+            .then((data) => { 
+                setData(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch(error => {
+                setIsLoading(false);
+                console.log(error.message);
+            })
+    }, [url])
+
+    return { data, isLoading, error}
+}
+
+export default useFetch;
