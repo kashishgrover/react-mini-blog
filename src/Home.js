10ce5a0e88f45f8b0856b2f45b558bed0cf07b83
Varsha Singh <varsha.singh0497@gmail.com>
2021-10-26 14:21:48 +0530

    feat: get data from json file

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -14,9 +14,19 @@ const Home = () => {
         setBlogs(newBlogs); 
     }
 
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then( res => {
+                return res.json();
+            })
+            .then((data) => { 
+                setBlogs(data);
+            })
+    }, [])
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> 
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/> }
         </div>
     );
 }
