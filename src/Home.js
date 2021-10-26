2f718eb3aa2b35d377d497fa01930b28686b61e1
Varsha Singh <varsha.singh0497@gmail.com>
2021-10-26 17:04:51 +0530

    feat: show error if the data cannot be fetched

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,31 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then( res => {
+                if(!res.ok) {
+                    throw Error('could notfetch data for that resource');
+                }
                 return res.json();
             })
             .then((data) => { 
                 setBlogs(data);
                 setIsLoading(false);
+                setError(null);
+            })
+            .catch(error => {
+                setIsLoading(false);
+                console.log(error.message);
             })
     }, [])
 
     return (
         <div className="home">
+            { error && <div>{ error }</div>}
             { isLoading && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs" /> }
         </div>
