e178c57b84aaa45fe135351485805030bed93155
Varsha Singh <varsha.singh0497@gmail.com>
2021-10-29 15:22:16 +0530

    feat: handle routing through browser

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,9 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+
+        fetch(url, { signal: abortCont.signal })
             .then( res => {
                 if(!res.ok) {
                     throw Error('could not fetch data for that resource');
@@ -19,9 +21,16 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(error => {
-                setIsLoading(false);
-                console.log(error.message);
+                if(error.name === 'Abort Error') {
+                    console.log('fetch aborted');
+                } else {
+                    setIsLoading(false);
+                    console.log(error.message);
+                }
             })
+
+            return () => abortCont.abort();
+
     }, [url])
 
     return { data, isLoading, error}
