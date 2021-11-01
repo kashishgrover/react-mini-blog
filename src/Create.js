e79ecb01bc2abec6427354bd50cf00ab5ab1b8b3
Varsha Singh <varsha.singh0497@gmail.com>
2021-11-01 19:31:51 +0530

    feat: save state of form as user adds input

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 const Create = () => {
+    const[title, setTitle] = useState('');
+    const[body, setBody] = useState('');
+    const[author, setAuthor] = useState('mario');
+    
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
             <form>
                 <label>Blog title</label>
-                <input type="text" required></input>
+                <input 
+                    type="text" 
+                    required 
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}></input>
                 <label>Blog body:</label>
-                <textarea required></textarea>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                    ></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
